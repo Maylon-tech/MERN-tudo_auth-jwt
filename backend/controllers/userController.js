95a815ed73dcc05546ae0012e4efcc032a77fde2
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-03-18 15:34:31 +0900

    connect to DB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,24 @@
+import asyncHandler from 'express-async-handler'
 
 
+// GET the profile data Info
+export const getUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message: "The User testing.. "})
+})
 
+// POST new data API request "register"
+export const createUser = asyncHandler(async (req, res) => {
+    console.log(req.body)
 
-export const getUser = async (req, res) => {
-    res.status(200).json({message: "The USer testing.. "})
-}
-
-
-export const createUser = async (req, res) => {
     res.status(200).json({message: "Creating User.. "})
-}
+})
 
-export const updateUser = async (req, res) => {
+// PUT Update from DB a new Data overWritting 
+export const updateUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update User ${req.params.id}`})
-}
+})
 
-
-export const deleteUser = async  (req, res) => {
+// DELETE Remove Data throw API on DB
+export const deleteUser = asyncHandler(async  (req, res) => {
     res.status(200).json({message: `Delete User ${req.params.id}`})
-}
+})
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 dotenv.config()
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use("/api/user", userRoutes)
-
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
