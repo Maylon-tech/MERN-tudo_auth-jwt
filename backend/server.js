fe7e44641376e99ea298fc7d24cd492a2e6602ad
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-03-19 05:46:00 +0900

    MOngodb Connection string start with mongoCompass.

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from 'express'
+import colors from 'colors'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
 import errorHandler from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
 const PORT = process.env.PORT || 5000
 
+connectDB()
 const app = express()
 dotenv.config()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
 app.use("/api/user", userRoutes)
 app.use(errorHandler)
 
