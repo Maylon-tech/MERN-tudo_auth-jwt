18813643ef5705d8395383ced04f57f8b167a45e
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-03-19 15:56:59 +0900

    REST API for User try

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,56 @@
 import asyncHandler from 'express-async-handler'
+import User from '../model/userModel.js'
 
 
 // GET the profile data Info
 export const getUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "The User testing.. "})
+    const user = await User.find()
+
+
+    res.status(200).json(user)
 })
 
 // POST new data API request "register"
 export const createUser = asyncHandler(async (req, res) => {
-    console.log(req.body)
+    if (!req.body.email || !req.body.password) {
+        res.status(400)
+        throw new Error("Please add a text field!")
+    }
 
-    res.status(200).json({message: "Creating User.. "})
+    const newUser = await User.create({
+        email: req.body.email,
+        password: req.body.password
+    })
+    res.status(200).json(newUser)
 })
 
 // PUT Update from DB a new Data overWritting 
 export const updateUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update User ${req.params.id}`})
+    // Busca o USER pelo ID para atualiza-lo
+    const user = await User.findById(req.params.id)
+    // Chekcar se existe mesmo
+    if (!user) {
+        res.status(400) 
+        throw new Error("User Profile not Found..!!")
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedUser)
 })
 
 // DELETE Remove Data throw API on DB
-export const deleteUser = asyncHandler(async  (req, res) => {
-    res.status(200).json({message: `Delete User ${req.params.id}`})
-})
+export const deleteUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)    
+    if (!user) {
+        res.status(400) 
+        throw new Error("User Profile not Found..!!")
+    }
+
+    const removeUser = await User.findByIdAndDelete(req.params.id, req.body, {
+        new: true,
+    })
+    res.status(200).json(removeUser)
+})  
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
 import express from 'express'
 import colors from 'colors'
 import dotenv from 'dotenv'
+
 import userRoutes from './routes/userRoutes.js'
 import errorHandler from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
 connectDB()
 const app = express()
-dotenv.config()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use("/api/user", userRoutes)
 app.use(errorHandler)
+
+app.use("/api/user", userRoutes)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
